3535125017ad6e42d0551f2871d4d8f3efb47caa
Jyotirmoy Paul <djyo.geos01@gmail.com>
2025-07-12 17:45:35 +0530

    update

---
+++ music/artist.js.js
@@ -0,0 +1,85 @@
+            artists: [
+                {
+                    id: "mohiuddin_dagar",
+                    name: "Ustad Mohiuddin Dagar",
+                    description: `
+                        <p>One of the most revered Dhrupad singers of the 20th century, known for his deep, resonant voice and spiritual approach to music.</p>
+                        <img class="content-image" src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTo3J_Gvf8rZ3QScew-MGKi9grMmeA3V05GPMVRj8cdrWrC67xagA6vis1sh3CoiMNpysu1LykaB8J--znC2dpyN9nF64lmGzCCH8KaU7NK" alt="Moinuddin and Aminuddin Dagar">
+                        <p class="image-caption">Ustad Moinuddin Dagar (right) with his brother Aminuddin Dagar</p>
+                    `,
+                    birth: "1920",
+                    death: "1966",
+                    contribution: "He was instrumental in keeping the Dagar tradition alive during difficult times and trained many disciples including his younger brother Zia Moinuddin Dagar.",
+                    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Moinuddin_and_Aminuddin_Dagar.jpg/320px-Moinuddin_and_Aminuddin_Dagar.jpg",
+                    ragas: [
+                        {
+                            name: "Yaman",
+                            url: "https://www.youtube.com/watch?v=q5trNs7M3MU",
+                            tags: ["yaman"]
+                        },
+                        {
+                            name: "Bhoop",
+                            url: "https://www.youtube.com/watch?v=example1",
+                            tags: ["bhoop"]
+                        }
+                    ],
+                    links: [
+                        { title: "Biography", url: "https://en.wikipedia.org/wiki/Moinuddin_Dagar" }
+                    ]
+                },
+
+                {
+                    id: "nancy_lesh",
+                    name: "Nancy lesh",
+                    description: `
+                        <p>One of the most revered Dhrupad singers of the 20th century, known for his deep, resonant voice and spiritual approach to music.</p>
+                        <img class="content-image" src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTo3J_Gvf8rZ3QScew-MGKi9grMmeA3V05GPMVRj8cdrWrC67xagA6vis1sh3CoiMNpysu1LykaB8J--znC2dpyN9nF64lmGzCCH8KaU7NK" alt="Moinuddin and Aminuddin Dagar">
+                        <p class="image-caption">Ustad Moinuddin Dagar (right) with his brother Aminuddin Dagar</p>
+                    `,
+                   
+                    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Moinuddin_and_Aminuddin_Dagar.jpg/320px-Moinuddin_and_Aminuddin_Dagar.jpg",
+                    ragas: [
+                        {
+                            name: "Yaman",
+                            url: "https://www.youtube.com/watch?v=q5trNs7M3MU",
+                            tags: ["yaman"]
+                        },
+                        {
+                            name: "Bhoop",
+                            url: "https://www.youtube.com/watch?v=example1",
+                            tags: ["bhoop"]
+                        }
+                    ],
+                    links: [
+                        { title: "Biography", url: "https://en.wikipedia.org/wiki/Moinuddin_Dagar" }
+                    ]
+                },
+
+                {
+                    id: "faiyaz_khan",
+                    name: "Faiyaz Khan",
+                    description: `
+                        <p>One of the most celebrated Indian vocalists of the Kirana gharana, known for his powerful voice and mastery over khayal singing.</p>
+                        <img class="content-image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Bhimsen_Joshi.jpg/320px-Bhimsen_Joshi.jpg" alt="Bhimsen Joshi">
+                        <p class="image-caption">Pandit Bhimsen Joshi in performance</p>
+                    `,
+                   
+                    image: "https://upload.wikimedia.org/wikipedia/commons/a/a8/FAIYAZ_KHAN.jpg",
+                    ragas: [
+                        {
+                            name: "Yaman",
+                            url: "https://www.youtube.com/watch?v=S0ioBZItZ2A",
+                            tags: ["yaman"]
+                        },
+                        {
+                            name: "Miyan ki Todi",
+                            url: "https://www.youtube.com/watch?v=example2",
+                            tags: ["todi"]
+                        }
+                    ],
+                    links: [
+                        { title: "Wikipedia", url: "https://en.wikipedia.org/wiki/Bhimsen_Joshi" }
+                    ]
+                },
+                // ... other artists ...
+            ],
