fd1c67dd371b8e19c05398ebdda1aba2c92949ae
Jyotirmoy Paul <djyo.geos01@gmail.com>
2026-09-23 04:52:38 +0530

    update

---
+++ layOut_Oslo/js/cv.js
@@ -0,0 +1,4 @@
+/* cv.js: behaviour used only on the CV page. */
+
+/* Page banner: same mantle-convection engine as the other pages (see /js/mantle.js). */
+MantleFlow(document.getElementById('mantle'), document.querySelector('.flow-toggle'));
---
+++ layOut_Oslo/js/resources.js
@@ -0,0 +1,5 @@
+/* resources.js: behaviour used only on the Resources page. */
+
+/* Page banner: same mantle-convection engine as the home, Research, Outreach and Teaching pages
+   (see /js/mantle.js). */
+MantleFlow(document.getElementById('mantle'), document.querySelector('.flow-toggle'));
